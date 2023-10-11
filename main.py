64ada01418df5a6b342aed0b4bc85a8908a2583d
Joseph-Ano <80886311+Joseph-Ano@users.noreply.github.com>
2023-10-11 15:46:55 +0800

    added function to parse logic section

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from utils import*
 
 def main():
     data, logic = get_inputs()
-    print(data)
-    print(logic)
-
+    # print(data)
+    # print(logic)
+    parse_logic_input(logic)
 main()
 
 
--- utils.py
+++ utils.py
@@ -21,6 +21,43 @@ def get_inputs():
 
     return data, logic
 
+def parse_logic_input(logic):
+    states = set()
+    language = set()
+    memory_language = {}
+    instructions = []
+
+    for line in logic:
+        components = line.split(" ")
+        symbolAndDest = components[2:]
+
+        for entry in symbolAndDest:
+            sourceState = components[0][0]
+            action = components[1] # may contain memory
+            memory = ""
+
+            idx = action.find('(')
+
+            if(idx != -1): # separates actions from the memory if present
+                memory = action[idx+1:-1]
+                action = action[:idx]
+
+            symbol = entry[1: entry.find(",")] # gets the symbol
+            dest = entry[entry.find(",")+1: -1] # gets the destination state
+
+            states.add(sourceState)
+
+            if(memory != ""): # if line uses memory, includ symbol as part of language for memory
+                memory_language.setdefault(memory, set()).add(symbol)
+            else: # include symbol as part of language
+                language.add(symbol)
+
+            instructions.append((sourceState, action, memory, symbol, dest))
+            
+    # print(memory_language)
+    return states, language, memory_language, instructions  
+    
+
 
 # def find_logic_section(inputs: list):
 #     for i in range(len(inputs)):
