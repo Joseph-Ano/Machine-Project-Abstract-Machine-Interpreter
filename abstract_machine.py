514e0c8cbdd4623a88b6a174cb332325b66b5d6a
Joseph-Ano <80886311+Joseph-Ano@users.noreply.github.com>
2023-10-11 20:17:49 +0800

    added scan function

--- abstract_machine.py
+++ abstract_machine.py
@@ -1,12 +1,54 @@
+from utils import *
+
 class abstract_machine:
-  def __init__(self, states, language, instructions, memory, input, curState, curInputIdx):
+  def __init__(self, states, language, instructions, memory, curState, action, input, curInputIdx):
     self.states = states
     self.language = language
-    self.transitions = instructions
+    self.instructions = instructions
     self.memory = memory
-    self.input = input
     self.curState = curState
+    self.action = action
+    self.input = input
     self.curInputIdx = curInputIdx
+    self.machine_stack = []
+    self.valid_instructions = get_valid_instructions(instructions, curState, input, curInputIdx)
 
   def scan(self):
-    pass
+    if(self.curInputIdx < len(self.input)-1):
+      for valid_instruction in self.valid_instructions:
+        next_state = valid_instruction[4]
+        next_action = ""
+
+        # to find the action of the next state
+        for instruction in self.instructions:
+          if next_state == instruction[0]:
+            next_action = instruction[1]
+            break
+
+        self.machine_stack.append(abstract_machine(
+          self.states,
+          self.language,
+          self.instructions,
+          valid_instruction[2],
+          next_state,
+          next_action,
+          self.input,
+          self.curInputIdx + 1
+        ))
+  
+    future_machine = self.machine_stack.pop()
+
+    self.memory = future_machine.memory
+    self.curState = future_machine.curState
+    self.action = future_machine.action
+    self.curInputIdx = future_machine.curInputIdx
+    self.valid_instructions = future_machine.valid_instructions
+
+  def run_machine(self):
+    if(self.action =="SCAN"):
+      self.scan()
+    else:
+      self.machine_stack.pop()
+      # self.scan()
+      # print(self)
+
--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from utils import*
+from abstract_machine import*
+
 
 def main():
     data, logic = get_machine_design()
     # print(data)
     # print(logic)
 
-    # states, language, memory_language, instructions = parse_logic_input(logic)
+    states, language, memory_language, instructions = parse_logic_input(logic)
     # valid_instructions = get_valid_instructions(instructions, 'E', '#1', 1)
     # print(valid_instructions)
 
+    machine = abstract_machine(states, language, instructions, "temp", instructions[0][0], instructions[0][1], "10", 0)
+    # machine.machine_stack.append(machine)
+    machine.run_machine()
+    print_machine(machine)
+    # print("\n")
+    # machine.run_machine()
+    # print_machine(machine)
+
 main()
+
 
 
